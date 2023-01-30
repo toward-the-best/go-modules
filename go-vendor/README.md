2891cd91a56c179a5d423a18d72dad04a4737078
toward-the-best <towardthebestprogrammer@gmail.com>
2023-01-31 00:10:19 +0900

    go mod vendor 사용법

---
+++ go-vendor/README.md
@@ -0,0 +1,2 @@
+# vendor사용법 정리
+## https://doitnow-man.tistory.com/entry/go-go-mod-vendor-%EC%82%AC%EC%9A%A9%EB%B2%95
---
+++ go-vendor/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import(
+	"fmt"
+    "github.com/sirupsen/logrus"
+)
+
+func main() {
+    fmt.Println("Hello")
+    logrus.Info("hello")
+}
+
